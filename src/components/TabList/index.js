584cc56f524eb12297e9912e2b6451a44a5cc4d7
praveen <praveenkumaronamala@gmail.com>
2023-04-08 07:46:27 +0000

    solution

---
+++ src/components/TabList/index.js
@@ -0,0 +1,22 @@
+import './index.css'
+
+const TabList = props => {
+  const {item, displayContent, activeTab} = props
+  const {tabId, displayText} = item
+  const applyItem = () => {
+    displayContent(tabId)
+  }
+  const changeColor = activeTab ? 'change' : null
+  return (
+    <li className="head-cont">
+      <button
+        type="button"
+        className={`table-head ${changeColor}`}
+        onClick={applyItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+export default TabList
---
+++ src/components/ThumbnailList/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const ThumbnailList = props => {
+  const {item, matchPair} = props
+  const {id, thumbnailUrl} = item
+  const samePic = () => {
+    matchPair(id)
+  }
+  return (
+    <li className="item-cont">
+      <button className="btn-thumb" type="button" onClick={samePic}>
+        <img src={thumbnailUrl} alt="thumbnail" className="thumbnail" />
+      </button>
+    </li>
+  )
+}
+export default ThumbnailList
